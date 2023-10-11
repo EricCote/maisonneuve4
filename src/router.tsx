d048459e9b6c39ecfe689587c0b9e35c928a8b71
Eric Cote <eric@coteexpert.com>
2023-10-11 06:55:09 -0400

    Routage

--- src/router.tsx
+++ src/router.tsx
@@ -1,20 +1,7 @@
 import Home from './homepage/Home';
-
-import FundamentalsFr from './decks/1-fundmentals.fr.mdx';
-import FundamentalsEn from './decks/1-fundmentals.en.mdx';
-import ComponentsFr from './decks/2-components.fr.mdx';
-import RouterFr from './decks/3-react-router.fr.mdx';
-import RouterEn from './decks/3-react-router.en.mdx';
-import StateFr from './decks/4-state.fr.mdx';
-import LoadingFr from './decks/5-loading.fr.mdx';
-import ManageStateFr from './decks/6-manage-state.fr.mdx';
-import AdvancedFr from './decks/7-advanced.fr.mdx';
-import InstallEn from './decks/install.en.mdx';
-import InstallFr from './decks/install.fr.mdx';
-import PlanFr from './decks/plan.fr.mdx';
-
 import GotoPopup from './components/GotoPopup';
 import { createBrowserRouter, Outlet, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const router = createBrowserRouter([
   {
@@ -40,21 +27,13 @@ const router = createBrowserRouter([
         ),
         children: [
           {
-            path: '/:lang?',
+            path: 'decks/:lang/',
             element: <Language />,
             children: [
-              { path: 'en/install', element: <InstallEn /> },
-              { path: 'fr/install', element: <InstallFr /> },
-              { path: 'fr/plan', element: <PlanFr /> },
-              { path: 'en/fundamentals', element: <FundamentalsEn /> },
-              { path: 'fr/fundamentals', element: <FundamentalsFr /> },
-              { path: 'fr/components', element: <ComponentsFr /> },
-              { path: 'en/react-router', element: <RouterEn /> },
-              { path: 'fr/react-router', element: <RouterFr /> },
-              { path: 'fr/state', element: <StateFr /> },
-              { path: 'fr/loading', element: <LoadingFr /> },
-              { path: 'fr/manage-state', element: <ManageStateFr /> },
-              { path: 'fr/advanced', element: <AdvancedFr /> },
+              {
+                path: ':id',
+                element: <MyLoader />,
+              },
             ],
           },
         ],
@@ -64,6 +43,20 @@ const router = createBrowserRouter([
 ]);
 
 export default router;
+
+function MyLoader() {
+  const { id, lang } = useParams();
+
+  const [jsx, setJsx] = useState(<></>);
+  useEffect(() => {
+    Loading();
+  });
+  async function Loading() {
+    const Result = await import(`./decks/${id}.${lang}.mdx`);
+    setJsx(Result.default);
+  }
+  return jsx;
+}
 
 function Language() {
   // Get the lang param from the URL.
